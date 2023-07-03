a91bc331b51097e7f13b61437ee25f0038ed032d
amanymqr <a.moqr20@gmail.com>
2023-07-04 00:01:46 +0300

    authentcation + verify by email msg

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
         public function index()
         {
-            return view('admin.index');
+                // dd(Auth::user()->type);
+                return view('admin.index');
         }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,23 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 
-
+//admin dashboard with authentiaction
 Route::prefix(LaravelLocalization::setLocale())->group(function ()
 {
-    Route::prefix('admin')->name('admin.')->group(function ()
+    Route::prefix('admin')->name('admin.')->middleware('auth','user_type','verified')->group(function ()
     {
+//  بيفحص هل مسجل دخول وكمان مش يوزر -أدمن
         Route::get('/', [AdminController::class, 'index'])->name('index');
     });
 });
+
+Auth::routes(['verify'=> true]);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::view('no-access','no_access');
+// Route::view('not-allowed', 'not_allowed');
